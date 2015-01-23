365e95185b50bf69de99859e8d011fd8e885e813
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2015-01-22 20:34:43 -0600

    Safeguard.

--- src/CoinToss.py
+++ src/CoinToss.py
@@ -92,6 +92,10 @@ elif sys.argv[1] == "-v":
 	print "CoinToss", version
 	sys.exit(0)
 
+if sys.argv[1] is not int:
+	print "Please give an integer."
+	sys.exit(0)
+
 printCopyright()
 
 # Create necessary variables
